0058654b1d5c793189775ba0f30a646c3074a636
caio giffoni <caio_cgf@hotmail.com>
2026-05-21 15:00:31 -0300

    fix: update CV link in config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const CONFIG = {
   LINKEDIN: "https://www.linkedin.com/in/caiocgfg",
   GITHUB: "https://github.com/caiogiffoni",
-  CV: "https://drive.google.com/uc?export=download&id=1Tc_EAm2xRMcYjPTDTnj8h6vBLLsKPKSn",
+  CV: "https://drive.google.com/uc?export=download&id=1yL0b_SZ7Kvvk46EIeeaV_EPIQhT0ezd3",
 };
 
 export default CONFIG;
